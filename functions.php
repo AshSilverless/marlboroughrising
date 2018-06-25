11f4034b32bb0b40e98868f687f1b9e329cb25e5
Ash - SIlverless <ash@silverless.co.uk>
2018-06-25 10:42:50 +0100

    Empty cart page

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 /*
 * Add your own functions here. You can also copy some of the theme functions into this file. 
-* Wordpress will use those functions instead of the original functions then.
+* Wordpress will use those functions instead of the original functions.
 */
 
 add_theme_support('avia_template_builder_custom_css');
@@ -10,5 +10,32 @@ add_theme_support('avia_template_builder_custom_css');
 add_filter( 'wc_empty_cart_message', 'custom_wc_empty_cart_message' );
 
 function custom_wc_empty_cart_message() {
+	
+	?>
+</div></div></div></div>
+
+
+	<div class="empty-cart-wrapper">
+
+<div class="container">
+	
+	<h1>Testing, testing. One, two...</h1>
+	<?php
   return "Oh dear!  You don't have any tickets in your cart.";
+  ?>
+  
+	</div>
+	</div>
+	
+	
+	
+	
+	
+	
+	
+	
+	<?php
+  
+  
+  
 }
