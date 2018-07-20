134afe1fef9efa5ef46268d00c25a6e385056108
Ash - SIlverless <ash@silverless.co.uk>
2018-07-20 09:20:13 +0100

    init 404

---
+++ 404.php
@@ -0,0 +1,33 @@
+<?php
+	if ( !defined('ABSPATH') ){ die(); }
+	
+	global $avia_config;
+
+	/*
+	 * get_header is a basic wordpress function, used to retrieve the header.php file in your theme directory.
+	 */
+	 get_header();
+
+
+	 echo avia_title(array('title' => __('Error 404 - page not found', 'avia_framework')));
+	 
+	 do_action( 'ava_after_main_title' );
+	?>
+
+
+		<div class='container_wrap container_wrap_first main_color <?php avia_layout_class( 'main' ); ?> fourohfour'>
+			
+			<div class='container'>
+
+<h1>Oh dear.  Looks like you've taken a wrong turn</h1>
+
+<p>Best not to look them in the eye and head back to safety by clicking here</p>
+
+			</div><!--end container-->
+
+		</div><!-- close default .container_wrap element -->
+
+
+
+
+<?php get_footer(); ?>
