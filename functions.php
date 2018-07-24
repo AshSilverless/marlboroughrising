6b6720b9adf914bba5d4dd993ff9baac6f189e3d
Ash - SIlverless <ash@silverless.co.uk>
2018-07-24 09:24:16 +0100

    Add FB Pixel - fixed

--- functions.php
+++ functions.php
@@ -30,4 +30,27 @@ function custom_wc_empty_cart_message() {
 	<?php
  
 }
-  
+
+/* ADD TO HEAD */
+add_action('wp_head', 'silverless_head');
+function silverless_head(){
+?>
+<!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '190286758053522');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=190286758053522&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+<?php
+}; 
