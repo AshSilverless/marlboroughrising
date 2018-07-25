3aa9122e3335dc5a0c59f0203808df3716e1c354
Ash - SIlverless <ash@silverless.co.uk>
2018-07-25 09:22:52 +0100

    Replace FB pixel code

--- functions.php
+++ functions.php
@@ -45,11 +45,11 @@ function silverless_head(){
   t.src=v;s=b.getElementsByTagName(e)[0];
   s.parentNode.insertBefore(t,s)}(window, document,'script',
   'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '190286758053522');
+  fbq('init', '2091356217603534');
   fbq('track', 'PageView');
 </script>
 <noscript><img height="1" width="1" style="display:none"
-  src="https://www.facebook.com/tr?id=190286758053522&ev=PageView&noscript=1"
+  src="https://www.facebook.com/tr?id=2091356217603534&ev=PageView&noscript=1"
 /></noscript>
 <!-- End Facebook Pixel Code -->
 
